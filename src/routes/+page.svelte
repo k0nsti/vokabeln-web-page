fa9704c5b01c1a93a4dbb29fb1954c0b3930f7e9
k0nsti <kkarpman@clickforit.de>
2024-02-12 15:07:57 +0100

    feat: add style change on true or false

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,37 @@
 <script>
-    let enteredword="";
+	let enteredword = '';
+	let color = '';
 	const words = { 1: { de: 'gut', en: 'good' }, 2: { de: 'süß', en: 'cute' } };
 	let wordD = words[2]['de'];
 	let w1 = words[2]['en'];
-	let answer="";
+	let answer = '';
 	function check(w2) {
 		console.log('xxx', w1, w2);
 		if (w1 == w2) {
 			answer = 'richtig';
+			color = 'green';
 		} else {
 			answer = 'falsch';
+			color = 'red';
 		}
 	}
 </script>
 
-<p>{wordD}</p>
+<div>
+	<p>{wordD}</p>
+</div>
+
+<p> tippe das Wort in Englisch ein und bestätige</p>
 <input bind:value={enteredword}/>
+
 <button on:click={check(enteredword)}> Bestätigen! </button>
-<p >{answer}</p>
+
+<div style="--theme-color: {color}">
+	<p>{answer}</p>
+</div>
+
+<style>
+	p {
+		color: var(--theme-color);
+	}
+</style>
