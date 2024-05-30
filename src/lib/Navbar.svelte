3959aa9441e278249fae87f0deea8efd0274f2cf
k0nsti <kkarpman@clickforit.de>
2024-05-30 16:40:48 +0200

    feat: change size/color of Navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,28 +1,31 @@
 <script>
-    export let activePage = '';
-  </script>
-  
-  <nav>
-    <ul>
-      <li><a href="/" class:active={activePage === 'home'}>Home</a></li>
-      <li><a href="/uebersetze" class:active={activePage === 'uebersetze'}>Übersetzen</a></li>
-    </ul>
-  </nav>
-  
-  <style>
-    nav ul {
-      display: flex;
-      list-style: none;
-      padding: 0;
-    }
-    nav ul li {
-      margin-right: 1rem;
-    }
-    nav ul li a {
-      text-decoration: none;
-    }
-    .active {
-      font-weight: bold;
-    }
-  </style>
-  
+	export let activePage = '';
+</script>
+
+<nav>
+	<ul>
+		<li><a href="/" class:active={activePage === 'home'}>Home</a></li>
+		<li><a href="/uebersetze" class:active={activePage === 'uebersetze'}>Übersetzen</a></li>
+	</ul>
+</nav>
+
+<style>
+	nav ul {
+		display: flex;
+		list-style: none;
+		padding: 0;
+		position: relative;
+		background-color: #24ca19;
+    height: 80px; /* Erhöht die Höhe der Navbar */
+    align-items: center; /* Zentriert die Listenelemente vertikal */
+	}
+	nav ul li {
+		margin-right: 1rem;
+	}
+	nav ul li a {
+		text-decoration: none;
+	}
+	.active {
+		font-weight: bold;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,2 @@
-<script>
-    import Navbar from '$lib/Navbar.svelte';
-  </script>
-  
-  <Navbar activePage="home" />
+
   <h1>Welcome to the Translate Game</h1>
