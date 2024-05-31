44dba24759f1aaa232fa43e6a858a1e4e58d6974
k0nsti <kkarpman@clickforit.de>
2024-06-01 00:16:10 +0200

    refactor

---
+++ src/lib/components/Uebersetze.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let data;
+	let enteredword = '';
+    let answer = '';
+	let color = '';
+
+	let wordD = data.deutsch;
+	let w1 = data.englisch;
+
+    function check(w2) {
+		if (w1 == w2) {
+			answer = 'richtig';
+			color = 'green';
+		} else {
+			answer = 'falsch';
+			color = 'red';
+		}
+	}
+
+</script>
+
+<div>
+	<p>{wordD}</p>
+</div>
+
+<p>tippe das Wort in Englisch ein und bestätige</p>
+<input bind:value={enteredword} />
+
+<button on:click={check(enteredword)}> Bestätigen! </button>
+
+<div style="--theme-color: {color}">
+	<p>{answer}</p>
+</div>
+
+<style>
+	p {
+		color: var(--theme-color);
+	}
+</style>
--- src/routes/uebersetze/+page.svelte
+++ src/routes/uebersetze/+page.svelte
@@ -1,36 +1,7 @@
 <script>
+	import Uebersetze from '$lib/components/Uebersetze.svelte';
 	export let data;
-	let enteredword = '';
-	let color = '';
-	let wordD = data.deutsch;
-	let w1 = data.englisch;
-	let answer = '';
-	function check(w2) {
-		if (w1 == w2) {
-			answer = 'richtig';
-			color = 'green';
-		} else {
-			answer = 'falsch';
-			color = 'red';
-		}
-	}
+
 </script>
 
-<div>
-	<p>{wordD}</p>
-</div>
-
-<p>tippe das Wort in Englisch ein und bestätige</p>
-<input bind:value={enteredword} />
-
-<button on:click={check(enteredword)}> Bestätigen! </button>
-
-<div style="--theme-color: {color}">
-	<p>{answer}</p>
-</div>
-
-<style>
-	p {
-		color: var(--theme-color);
-	}
-</style>
+<Uebersetze {data} />
